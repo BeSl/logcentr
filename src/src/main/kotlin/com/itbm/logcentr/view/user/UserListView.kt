2f8fa216c99957745fad337119b99ef96d0aedad
BeSl <belyakovva@me.com>
2025-01-21 16:06:24 +0300

    init jmix project

---
+++ src/src/main/kotlin/com/itbm/logcentr/view/user/UserListView.kt
@@ -0,0 +1,13 @@
+package com.itbm.logcentr.view.user
+
+import com.itbm.logcentr.entity.User
+import com.itbm.logcentr.view.main.MainView
+import com.vaadin.flow.router.Route
+import io.jmix.flowui.view.*
+
+@Route(value = "users", layout = MainView::class)
+@ViewController(id = "User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+open class UserListView : StandardListView<User>()
