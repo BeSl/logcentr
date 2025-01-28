bfdc8f90dfabeb7ab080f99f8e271229ece687e1
BeSl <belyakovva@me.com>
2025-01-28 14:09:36 +0300

    ридми

--- README.md
+++ README.md
@@ -18,3 +18,69 @@
 4. определить роли в системе
 5. реализовать хранение редактирование подключений к серверам КХ
 6. ДТО для загрузки таблицы журнала
+
+
+
+# Подготовка окружения
+
+## Установка ClickHouse
+
+- Пример docker-compose.yml
+```yaml
+version: '3.7'
+services:
+ ClickHouse-Server:
+   image: yandex/clickhouse-server
+   container_name: ClickHouse
+   ports:
+     - "8123:8123"
+     - "9000:9000"
+   volumes:
+     - ./db:/var/lib/clickhouse
+
+```
+
+- Запросы для создания таблиц
+```sql
+CREATE DATABASE IF NOT EXISTS ch_test1_base;
+ 
+CREATE TABLE IF NOT EXISTS ch_test1_base.eventlog_error(
+   `timestamp` DateTime,
+   `source_type` String,
+   `host` String,
+   `message` String,
+   `component_id` String,
+   `component_kind` String,
+   `component_type` String,
+   `reason` String,
+   `error` String
+) ENGINE = MergeTree() PARTITION BY toYYYYMM(timestamp)
+ORDER BY (timestamp, source_type, host);
+ 
+CREATE TABLE IF NOT EXISTS ch_test1_base.eventlog(
+    `DateTime` DateTime,
+    `TransactionStatus` String,
+    `TransactionDate` DateTime,
+    `TransactionNumber` Int32,
+    `User` String,
+    `UserUuid` String,
+    `Computer` String,
+    `Application` String,
+    `Connection` Int32,
+    `Event` String,
+    `Severity` String,
+    `Comment` String,
+    `Metadata` String,
+    `MetadataUuid` String,
+    `Data` String,
+    `DataPresentation` String,
+    `Server` String,
+    `MainPort` Int16,
+    `AddPort` Int16,
+    `Session` Int32
+) ENGINE = MergeTree() PARTITION BY toYYYYMM(DateTime)
+ORDER BY (DateTime);
+
+```
+Структура взята как образец хранилища логов журналов регистраций из 1С
+Под эту структуру таблиц ориентируемся при разработке DTO объектов и просмотре журналов.
--- src/src/main/kotlin/com/itbm/logcentr/view/extservers/ExtServersDetailView.kt
+++ src/src/main/kotlin/com/itbm/logcentr/view/extservers/ExtServersDetailView.kt
@@ -3,6 +3,7 @@ package com.itbm.logcentr.view.extservers
 import com.itbm.logcentr.entity.ExtServers
 import com.itbm.logcentr.view.main.MainView
 import com.vaadin.flow.component.ClickEvent
+import com.vaadin.flow.component.combobox.ComboBox
 import com.vaadin.flow.component.html.Span
 import com.vaadin.flow.component.textfield.TextField
 import com.vaadin.flow.router.Route
@@ -11,6 +12,7 @@ import io.jmix.flowui.kit.component.button.JmixButton
 import io.jmix.flowui.view.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody
 import org.springframework.beans.factory.annotation.Autowired
 
 @Route(value = "extServerses/:id", layout = MainView::class)
@@ -27,6 +29,9 @@ class ExtServersDetailView : StandardDetailView<ExtServers>() {
     @ViewComponent
     private lateinit var portField: TextField
 
+    @ViewComponent
+    private lateinit var dbNameField: ComboBox<String>
+
     @Subscribe(id = "pingButton", subject = "clickListener")
     private fun onPingButtonClick(event: ClickEvent<JmixButton>) {
         val client = OkHttpClient()
@@ -35,13 +40,25 @@ class ExtServersDetailView : StandardDetailView<ExtServers>() {
         {
             return
         }
-
+        val urlServer = "http://${hostField.value}:${portField.value}"
         val request = Request.Builder()
-            .url("http://${hostField.value}:${portField.value}")
+            .url(urlServer)
             .build()
 
         val response = client.newCall(request).execute()
         notifications?.show("Result ${response.code} - ${response.body?.string()}");
+        if (response.code == 200){
+            dbNameField.setItems(ListDBServer(urlServer))
+        }
         response.close()
+    }
+
+    fun ListDBServer(url: String):List<String> {
+        
+        val Body = RequestBody.create(okhttp3.MediaType.parse("application/json; charset=utf-8"), url)
+        val r = Request.Builder()
+            .url(url)
+            .build()
+
     }
 }
